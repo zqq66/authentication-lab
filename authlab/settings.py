2bea5f435475099277eab6440f195a61e335a2af
Alexander Wong <alex@udia.ca>
2019-02-20 18:43:30 -0700

    enabled token authentication

--- authlab/settings.py
+++ authlab/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'client.apps.ClientConfig',
 ]
 
@@ -128,5 +129,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     )
 }
